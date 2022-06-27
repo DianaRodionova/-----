0d074115adf9a69d504f9a3df98f75b8c3220b0e
DianaRodionova <dianka225.df@gmail.com>
2022-06-27 19:19:03 +0300

    добавление обязательных полей

--- js/modal.js
+++ js/modal.js
@@ -1,13 +1,25 @@
 const contactsLink = document.querySelector(".button-contacts");
 const contactsPopup = document.querySelector(".appointment");
 const modalClose = contactsPopup.querySelector(".close-button");
+const form = contactsPopup.querySelector(".appointment-form");
+const namePopup = contactsPopup.querySelector("[name=name]");
+const emailPopup = contactsPopup.querySelector("[name=email]");
 
 contactsLink.addEventListener("click", function (evt) {
   evt.preventDefault();
   contactsPopup.classList.add("modal-show");
+  namePopup.focus();
 });
 
 modalClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   contactsPopup.classList.remove("modal-show");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!namePopup.value || !emailPopup.value) {
+    evt.preventDefault();
+  } else {
+    localStorage.setItem("name", namePopup.value);
+  }
 });
